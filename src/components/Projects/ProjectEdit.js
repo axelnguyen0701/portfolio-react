5c6c6a2ea208a4f71a124e1440985604546fd8a6
hieu nguyen <hieu@hieu-HP-server.ed.shawcable.net>
2023-03-17 12:36:49 -0600

    only redirect if upload is complete

--- src/components/Projects/ProjectEdit.js
+++ src/components/Projects/ProjectEdit.js
@@ -6,31 +6,85 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../services/auth";
 import UnauthPage from "../Error/UnauthPage";
 import MarkdownEditor from "../Markdown/MarkdownEditor";
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from "firebase/storage";
+import app from "../../services/firstore";
 
 export default function ProjectEdit() {
     const project = useLoaderData();
     const [user, loading, error] = useAuthState(auth);
     const [name, setName] = useState(project.name);
     const [description, setDescription] = useState(project.description);
+    const [selectedFile, setSelectedFile] = useState();
+    const [percent, setPercent] = useState(0);
+
     const [link, setLink] = useState(project.link);
     // eslint-disable-next-line no-unused-vars
     const [url, setUrl] = useState(project.url);
     const navigate = useNavigate();
 
+    const uploadFile = () => {
+        const storage = getStorage(app);
+        //Get the ref first
+        const storageRef = ref(storage, `/files/${selectedFile.name}`);
+        //Create task
+        const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+
+        //Listener
+        uploadTask.on(
+            "state_chaqnged",
+            (snapshot) => {
+                const percent = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+
+                // update progress
+                setPercent(percent);
+                console.log(percent);
+            },
+            (err) => console.log(err),
+            () => {
+                // download url
+                getDownloadURL(uploadTask.snapshot.ref).then(async (_url) => {
+                    //After get the link then return the img url
+                    await updateProject({
+                        name,
+                        description,
+                        link,
+                        url: _url,
+                        id: project.id,
+                    });
+                    setName("");
+                    setDescription("");
+                    setLink("");
+                    navigate("/project");
+                });
+            }
+        );
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await updateProject({
-            name,
-            description,
-            link,
-            url,
-            id: project.id,
-        });
 
-        setName("");
-        setDescription("");
-        setLink("");
-        navigate("/project");
+        if (!!selectedFile) {
+            uploadFile();
+        } else {
+            await updateProject({
+                name,
+                description,
+                link,
+                url,
+                id: project.id,
+            });
+            setName("");
+            setDescription("");
+            setLink("");
+            navigate("/project");
+        }
     };
 
     if (!user) {
@@ -70,7 +124,12 @@ export default function ProjectEdit() {
                 <Form.Group controlId="formFile" className="mb-3">
                     <Image src={url} style={{ maxWidth: "100%" }} />
                     <Form.Label>Image of the project</Form.Label>
-                    <Form.Control type="file" accept="/image/*" />
+                    <Form.Control
+                        onChange={(e) => setSelectedFile(e.target.files[0])}
+                        type="file"
+                        accept="/image/*"
+                    />
+                    <div>{percent}</div>
                 </Form.Group>
 
                 <Form.Group>
