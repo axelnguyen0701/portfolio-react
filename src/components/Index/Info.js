27374f8001282bf376550d34ce7397fcd2debd2a
hieu nguyen <hieu@hieu-HP-server.ed.shawcable.net>
2023-03-17 13:00:18 -0600

    Add typing effect

--- src/components/Index/Info.js
+++ src/components/Index/Info.js
@@ -2,7 +2,7 @@ import { Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "./ava.png";
 import background from "./background.svg";
-
+import "./Info.css";
 export default function Info() {
     return (
         <Container
@@ -11,12 +11,13 @@ export default function Info() {
                 backgroundSize: "cover",
             }}
             fluid
-            className="h-100 d-flex flex-column justify-content-between"
+            className="h-100 d-flex flex-column justify-content-between typewriter"
         >
             <div>Hello I am</div>
             <h1>Axel Nguyen</h1>
             <h2>Full-stack developer</h2>
             <p>I fancy creating things from thin air</p>
+
             <Button
                 style={{ maxWidth: "25%", alignSelf: "center" }}
                 as={Link}
