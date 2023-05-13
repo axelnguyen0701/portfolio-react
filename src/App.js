141093d04cbab26ef085ab6f6db6c203419cf9f8
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-05-13 13:50:21 +0700

    add theme context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Outlet } from "react-router-dom";
+import { ThemeContext } from "./context/themeContext";
 function App() {
+    const [theme, setTheme] = useState("dark");
     return (
-        <div className="App">
-            <NavBar />
-            <Outlet />
-        </div>
+        <ThemeContext.Provider value={theme}>
+            <div className="App">
+                <NavBar />
+                <Outlet />
+            </div>
+        </ThemeContext.Provider>
     );
 }
 
