d591a154f326a6852e23a839986a42d52c37ca63
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-05-16 16:55:50 +0700

    Add spinner when loading

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { ThemeContext } from "./context/themeContext";
+import { Spinner } from "react-bootstrap";
 function App() {
     const [theme, setTheme] = useState("dark");
+    const { state } = useNavigation();
+
+    const renderLoadingSpinner = () => {
+        if (state === "loading")
+            return (
+                <>
+                    <Spinner role="status" animation="grow">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                    <div>Loading...</div>
+                </>
+            );
+        return <Outlet />;
+    };
+
     return (
         <ThemeContext.Provider value={theme}>
             <div className="App">
                 <NavBar setTheme={setTheme} />
-                <Outlet />
+                {renderLoadingSpinner()}
             </div>
         </ThemeContext.Provider>
     );
